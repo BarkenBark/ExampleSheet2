7622e65f40a3f2c02d5f44e9d90db959d6f4a356
mpbejmer <mpbejmer@gmail.com>
2017-09-26 16:45:26 +0200

    completed GenerateWeights and NeighboringFunctions

--- KohonenNetwork/NeighboringFunction.m
+++ KohonenNetwork/NeighboringFunction.m
@@ -1,7 +1,8 @@
-function val = NeighboringFunction(i1, i2)
+function value = NeighboringFunction(i1, i2, sigma)
 %NeighboringFunction Returns a value in the range (0,1] where larger values
 %indicate that i1 and i2 are topologically close in the output space.
-
+    
+    value = exp(-(i1-i2)^2/(2*sigma^2));
 
 end
 
