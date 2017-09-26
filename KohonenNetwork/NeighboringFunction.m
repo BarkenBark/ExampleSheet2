73f6416eb437e42ca41242408818adf42a102777
BarkenBark <oscar.henrik.bark@gmail.com>
2017-09-26 15:48:55 +0200

    NeighboringFunction.m

---
+++ KohonenNetwork/NeighboringFunction.m
@@ -0,0 +1,7 @@
+function val = NeighboringFunction(i1, i2)
+%NeighboringFunction Returns a value in the range (0,1] where larger values
+%indicate that i1 and i2 are topologically close in the output space.
+
+
+end
+
