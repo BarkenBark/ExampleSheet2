1d97ccf70e1d5aa52ded689f4069d681a7ac36bf
BarkenBark <oscar.henrik.bark@gmail.com>
2017-09-26 17:10:45 +0200

    Skrev UpdateWeights

--- KohonenNetwork/UpdateWeights.m
+++ KohonenNetwork/UpdateWeights.m
@@ -1,7 +1,17 @@
 function newWeights = UpdateWeights(weights, winningNeuron, pattern, learningRate, sigma)
 %UpdateWeights Update weights according to the Kohenen rule
-
   
+  newWeights = zeros(size(weights));
+  nbrOfNodes = size(weights, 1);
+  i0 = winningNeuron;
 
+  for i = 1:nbrOfNodes
+    weightVector = weights(i,:)'; %inputSize x 1
+    deltaWeightVector = learningRate*NeighboringFunction(i, i0, sigma)...
+      *(pattern-weightVector);
+    newWeightVector = weightVector + deltaWeightVector;
+    newWeights(i,:) = newWeightVector';
+  end
+  
 end
 
