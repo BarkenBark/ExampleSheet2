6749a7774a54f014ab8139bd32f0178f6f3ee685
BarkenBark <oscar.henrik.bark@gmail.com>
2017-09-27 10:09:53 +0200

    Skrev FindWinningNeuron

--- KohonenNetwork/FindWinningNeuron.m
+++ KohonenNetwork/FindWinningNeuron.m
@@ -1,7 +1,15 @@
-function [ output_args ] = FindWinningNeuron( input_args )
-%UNTITLED5 Summary of this function goes here
-%   Detailed explanation goes here
+function winningNeuron = FindWinningNeuron(pattern, weights)
+%FindWinningNeuron Returns the winning neuron index, which is the neuron
+%whose associated weight vector is closest to the patterns
 
+  distance = zeros(nbrOfNode,1);
+
+  for i = 1:nbrOfNodes
+    weightVector = weights(i,:)';
+    distance(i) = norm(pattern - weightVector);
+  end
+  
+  [~,winningNeuron] = min(distance);
 
 end
 
