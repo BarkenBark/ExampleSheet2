82d68adf991f7500fcb172f25ff82f491cfef70e
BarkenBark <oscar.henrik.bark@gmail.com>
2017-09-27 15:28:20 +0200

    CenterComponents

--- OjaLearning/CenterComponents.m
+++ OjaLearning/CenterComponents.m
@@ -1,7 +1,12 @@
-function centeredData = CenterComponents(data)
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
+function centeredData = CenterComponents(data, mode)
+%CenterComponents Mode = 'row' or 'column' => Center data along row or
+%column
 
+  if strcmp(mode, 'row')
+    centeredData = zscore(data, 0, 2);
+  elseif strcmp(mode, 'column')
+    centeredData = zscore(data, 0, 1);
+  end
 
 end
 
