e62352e9fd9e899d7dda47f501dab64fff3808f7
BarkenBark <oscar.henrik.bark@gmail.com>
2017-09-27 10:42:57 +0200

    dheiudh2

--- KohonenNetwork/Main.m
+++ KohonenNetwork/Main.m
@@ -2,7 +2,7 @@
 
 T_ORDER = 10^3;
 T_CONV = 2*10^4;
-NUMBER_OF_ITERATIONS = T_ODER + T_CONV;
+NUMBER_OF_ITERATIONS = T_ORDER + T_CONV;
 
 nbrOfPatterns = 1000; %p
 outputDimension = 100; %M
@@ -11,15 +11,16 @@ sigma_initial = 100;
 learningRate_initial = 0.1;
 tauSigma = 300;
 
-patterns = GeneratePatterns(nbrOfPatters); %Return 2 x nbrOfPatterns matrix
+patterns = GeneratePatterns(nbrOfPatterns); %Return 2 x nbrOfPatterns matrix
 inputDimension = size(patterns, 1); %N
-weights = GenerateWeights(inputDimension, outputDimension); %Return outputDimension x inputDimension matrix
+weightInterval = [-1, 1];
+weights = GenerateWeights(inputDimension, outputDimension, weightInterval); %Return outputDimension x inputDimension matrix
 
 for iteration = 1:NUMBER_OF_ITERATIONS
   
   if iteration <= T_ORDER
-    learningRate = TimeDependentEta(t, learningRate_initial, tauSigma);
-    sigma = TimeDependentSigma(t, sigma_initial, tauSigma);
+    learningRate = TimeDependentEta(iteration, learningRate_initial, tauSigma);
+    sigma = TimeDependentSigma(iteration, sigma_initial, tauSigma);
   else
     learningRate = 0.01;
     sigma = 0.9;
