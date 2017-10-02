7f5272ba367ff172a6e9fb432c2c51e560f81a54
mpbejmer <mpbejmer@gmail.com>
2017-10-02 18:37:09 +0200

    k

--- CompetitiveLearning/Main.m
+++ CompetitiveLearning/Main.m
@@ -7,10 +7,12 @@ targetOutputs = data(:,1)';
 %scatter(patterns(:,1), patterns(:,2))
 
 k = 100;
-networkDimensions = [2 k];
+nbrOfOutputNeurons = size(targetOutputs, 1);
+networkDimensions = [nbrOfOutputNeurons k];
 weightInterval = [-1 1];
 thresholdInterval = [-1 1];
 beta = 1/2;
+TRAINING_STEPS = 3000;
 
 nbrOfPatterns = size(patterns, 1);
 
@@ -21,21 +23,27 @@ transformedPatterns = TransformPatterns(patterns, k);
 weights = InitializeWeights(networkDimensions, weightInterval);
 thresholds = InitializeThresholds(networkDimensions, thresholdInterval);
 
-for iSupervisedIterations = 1:NUMBER_OF_ITERATIONS
+for iSupervisedIterations = 1:TRAINING_STEPS
     
-    thisPattern = transFormedPatterns(:,randi(nbrOfPatterns));
+    iRandom = randi(nbrOfPatterns);
+    thisPattern = transFormedPatterns(:,iRandom);
+    zeta = targetOutputs(iRandom);
+    
     output = weights*thisPattern - thresholds;
     output = TanActivation(output, beta);
-     = ForwardPropagate(thisPattern, weights);
-    weights = BackPropStep()
+    
+    %Update weights
+    deltaWeight = zeros(1,k);
+    for j=1:size(weights,2)
+        deltaWeight(1,j) = learningRate*beta*(zeta - output)*(1-output^2)*input(j,1);
+        weights(1,j) = weights(1,j) + deltaWeight(1,j);
+    end
+    
+    %Update Biases
+    deltaBiases = learningRate*beta*(zeta-output)*(1-output^2);
+    biases = biases - deltaBiases;
     
 end
-
-
-
-input = transformedPatterns
-
-%nbrOfNeurons = 
 
 
 
